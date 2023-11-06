1f6b5ac31b0d70a6bb314e6b4782ee40d316a6dc
Joao Pedro <joaochecco@gmail.com>
2023-11-06 20:28:15 -0300

    tela de login correto pronta

--- app/src/main/java/com/example/login/LoginCorretoActivity.kt
+++ app/src/main/java/com/example/login/LoginCorretoActivity.kt
@@ -2,10 +2,17 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginCorretoActivity : AppCompatActivity() {
+    private lateinit var textMensagem: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_correto)
+
+        textMensagem = findViewById(R.id.textMensagem)
+        val login = intent.getStringExtra("login")
+
+        textMensagem.text = "Bem-vindo(a), ${login}!"
     }
 }
